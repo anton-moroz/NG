9f8fbe0c743f70b3a657e231c354855fc43fe2bf
anton <anton.moroz@hotmail.com>
2021-02-10 14:06:59 +0200

    finished with task with Strategy pattern

---
+++ Patterns/Strategy/Strategy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Strategy;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MovementStrategyClass movement = new MovementStrategyClass();
+            movement.SetFuel(80);
+
+            movement.SetMovementStrategy(new ElectricityStrategy());
+            movement.SetMovementStrategy(new GasStrategy());
+            movement.SetMovementStrategy(new BenzinStrategy());
+
+            Console.WriteLine("\nTap Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
