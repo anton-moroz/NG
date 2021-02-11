298755924f7a3d500f7e045d083918568ab149e3
anton <anton.moroz@hotmail.com>
2021-02-11 23:29:00 +0200

    finished with task #3

---
+++ Patterns/AbstractFactory/AbstractFactory/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie = new Movie();
+
+            movie.SetLanguage("English");
+
+            Console.WriteLine();
+
+            movie.SetLanguage("Ukrainian");
+            Console.WriteLine("\nTap Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
